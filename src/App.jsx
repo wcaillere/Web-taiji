ce70b27eaf8bbba217c04c82a5d4101ad1637120
William Caillere <wcaillere.pro@gmail.com>
2023-02-09 10:03:06 +0100

    Initialisation du composant Puzzle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Puzzle from "./components/Puzzle/Puzzle";
 
 function App() {
   return (
     <div className="App">
       <h1>test</h1>
+      <Puzzle id="54" />
     </div>
   );
 }
---
+++ src/components/Puzzle/Puzzle.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Puzzle(props) {
+  return <div>Puzzle {props.id}</div>;
+}
